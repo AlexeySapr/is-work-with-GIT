815c04c8b0714e29951bd581a13a1d94378ac554
Alexey Sapr <alexeysapr@gmail.com>
2022-04-07 11:00:18 +0300

    Done task1

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,33 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(str) {
+  const strArr = str.split('').filter(el => el !== ' ');
+  const operatorIndex = strArr.findIndex(el => el !== '.');
+
+  const firstNumber = strArr.indexOf(strArr[operatorIndex]);
+  const secondNumber =
+    strArr.length - 1 - strArr.lastIndexOf(strArr[operatorIndex]);
+
+  let resNumber = 0;
+  switch (strArr[operatorIndex]) {
+    case '+':
+      resNumber = firstNumber + secondNumber;
+      break;
+    case '-':
+      resNumber = firstNumber - secondNumber;
+      break;
+    case '*':
+      resNumber = firstNumber * secondNumber;
+      break;
+    case '/':
+      resNumber = Math.floor(firstNumber / secondNumber);
+      break;
+
+    default:
+      break;
+  }
+  if (resNumber === 0) {
+    return '';
+  }
+  return '.'.repeat(resNumber);
 }
+
+console.log('result: ', calculate('..... - .'));
